7fba62566de3729c017fe748daaa4a812baee893
key <key@keys-MBP.lan>
2024-05-09 23:32:38 -0400

    initial

---
+++ Fastfood.Web/FasFood.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+
+
+		[Required]
+		public string Title { get; set; }
+		public Category()
+		{
+		}
+	}
+}
+
---
+++ Fastfood.Web/Fastfood.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fastfood.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
